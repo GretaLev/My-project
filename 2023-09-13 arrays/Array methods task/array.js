32a60f0e2d3f7297266676b8f3930bc3b1ac8869
GretaLev <levinskaite.greta@gmail.com>
2023-09-14 15:38:50 +0300

    Array methods task

--- 2023-09-13 arrays/Array methods task/array.js
+++ 2023-09-13 arrays/Array methods task/array.js
@@ -26,157 +26,157 @@
 // Atlikus šias užduotis eilės tvarka masyvas turėtų atrodyti taip:
 // ['start', 2, 3, 5, 11, 155, 888, '15x', 6789, -5564, 'obuolys', -51, 55, 0, 33, 789, 6543, 1, 'trylika', 444, 321, 654, -1, 987, -333, -321, 'end']
 
-// let array = [
-//   4,
-//   5,
-//   -5556,
-//   155,
-//   640,
-//   "15x",
-//   6789,
-//   -5564,
-//   478,
-//   654,
-//   "obuolys",
-//   789,
-//   -51,
-//   55,
-//   -222,
-//   0,
-//   -357,
-//   -56,
-//   "trylika",
-//   444,
-//   7778,
-//   4154,
-//   4751,
-// ];
+let array = [
+  4,
+  5,
+  -5556,
+  155,
+  640,
+  "15x",
+  6789,
+  -5564,
+  478,
+  654,
+  "obuolys",
+  789,
+  -51,
+  55,
+  -222,
+  0,
+  -357,
+  -56,
+  "trylika",
+  444,
+  7778,
+  4154,
+  4751,
+];
 
-// console.log(array);
+console.log(array);
 
 // // 1.1. Pašalinti pirmą masyvo narį.
 
-// array.shift();
+array.shift();
 
-// console.log(array);
+console.log(array);
 
-// let first = array.slice(0, 1);
-// console.log(first);
+let first = array.slice(0, 1);
+console.log(first);
 
 // // 1.2. Pašalinti paskutinį masyvo narį.
 
-// array.pop();
+array.pop();
 
-// console.log(array);
+console.log(array);
 
 // // 1.3. Rasti vidurinį masyvo narį ir jį pašalinti.
 
-// let middleIndex = Math.floor(array.length / 2);
-// array.splice(middleIndex, 1);
+let middleIndex = Math.floor(array.length / 2);
+array.splice(middleIndex, 1);
 
-// console.log(array);
+console.log(array);
 
 // // 1.4. Pašalinti priešpaskutinį masyvo narį.
 
-// array.splice(-2, 1);
+array.splice(-2, 1);
 
-// console.log(array);
+console.log(array);
 
 // // 1.5. Pašalinti antrą masyvo narį.
 
-// array.splice(1, 1);
+array.splice(1, 1);
 
-// console.log(array);
+console.log(array);
 
 // // 1.6. Pašalinti 7 ir 8 masyvo narius.
 
-// array.splice(6, 2);
+array.splice(6, 2);
 
-// console.log(array);
+console.log(array);
 
 // // 1.7. Pašalinti 4, 5 ir 6 masyvo narius skaičiuojant nuo galo.
 
-// array.splice(-6, 3);
+array.splice(-6, 3);
 
-// console.log(array);
+console.log(array);
 
 // // 1.8. Pašalinti trečią masyvo narį ir į jo vietą įdėti skaičių 888.
 
-// array.splice(2, 1, 888);
+array.splice(2, 1, 888);
 
-// console.log(array);
+console.log(array);
 
 // // 1.9. Pašalinti dešimtą masyvo narį ir į jo vietą įdėti skaičius: 33, 789 ir 6543.
 
-// array.splice(9, 1, 33, 789, 6543);
+array.splice(9, 1, 33, 789, 6543);
 
-// console.log(array);
+console.log(array);
 
 // // 1.10. Pašalinti paskutinį masyvo narį į jo vietą įdėti skaičius: 321, 654, 987.
 
-// array.splice(-1, 1, 321, 654, 987);
+array.splice(-1, 1, 321, 654, 987);
 
-// console.log(array);
+console.log(array);
 
 // // 1.11. Tarp pirmo ir antro masyvo narių įdėti skaičių 11.
 
-// array.splice(1, 0, 11);
+array.splice(1, 0, 11);
 
-// console.log(array);
+console.log(array);
 
 // // 1.12. Tarp 13 ir 14 masyvo narių įdėti skaičių 1.
 
-// array.splice(13, 0, 1);
+array.splice(13, 0, 1);
 
-// console.log(array);
+console.log(array);
 
 // // 1.13. Tarp paskutinio ir priešpaskutinio masyvo narių įdėti skaičių -1.
 
-// array.splice(-1, 0, -1);
+array.splice(-1, 0, -1);
 
-// console.log(array);
+console.log(array);
 
 // // 1.14. Į masyvo pradžią pridėti skaičius: 1, 2, 3.
 
-// array.unshift(1, 2, 3);
+array.unshift(1, 2, 3);
 
-// console.log(array);
+console.log(array);
 
 // // 1.15. Į masyvo pabaigą pridėti skaičius: -333, -321, -312.
 
-// array.push(-333, -321, -312);
+array.push(-333, -321, -312);
 
-// console.log(array);
+console.log(array);
 
 // // 1.16. Į masyvo vidurį pridėti skaičių 0.
 
-// array.splice(13, 0, 0);
+array.splice(13, 0, 0);
 
-// console.log(array);
+console.log(array);
 
 // // 1.17. Pašalinti pirmą masyvo narį.
 
-// array.shift();
+array.shift();
 
-// console.log(array);
+console.log(array);
 
 // // 1.18. Pašalinti paskutinį masyvo narį.
 
-// array.pop();
+array.pop();
 
-// console.log(array);
+console.log(array);
 
 // // 1.19. Į masyvo pradžią pridėti žodį "start".
 
-// array.unshift("start");
+array.unshift("start");
 
-// console.log(array);
+console.log(array);
 
 // // 1.20. Į masyvo pabaigą pridėti žodį "end".
 
-// array.push("end");
+array.push("end");
 
-// console.log(array);
+console.log(array);
 
 // 2. Tęsiant darbą su šiuo masyvu atlikti šias užduotis naudojant slice metodą. Kiekvienai užduočiai sukurti naują kintamąjį, jame išsaugoti reikšmę, kurią grąžina slice metodas, ir šį kintamąjį išvesti į konsolę:
 // 2.1. Gauti pirmą masyvo narį.
