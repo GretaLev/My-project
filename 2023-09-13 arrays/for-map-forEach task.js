d444c66b31d0d4b60b4ebe88c39a3d1a89d38e6c
GretaLev <levinskaite.greta@gmail.com>
2023-09-21 21:24:11 +0300

    For ciklas, map ir forEach metodai

--- 2023-09-13 arrays/for-map-forEach task.js
+++ 2023-09-13 arrays/for-map-forEach task.js
@@ -56,37 +56,51 @@ console.log(array);
 // 4. Tęsiant darbą su ankstesniu masyvu atlikti šias užduotis naudojant for ciklą ir map/forEach metodą. Kiekvieną užduotį atlikti abiem būdais (for ciklu ir map/forEach metodu). Visus narius išvesti į konsole pagal žemiau pateiktas sąlygas:
 // 4.1. Tik skaičius (number tipo duomenis).
 
-let ul41 = document.querySelector("#task-41");
-let list41 = document.createElement("li");
-ul41.append(list41);
+function task(array) {
+  for (let i = 0; i < array.length; i++) {
+    let item = array[i];
+    if (!isNaN(item)) {
+      let ul41 = document.querySelector("#task-41");
+      let liElement = document.createElement("li");
+      liElement.textContent = item;
+      ul41.append(liElement);
+    }
+  }
+}
+task(array);
+
+// function taskMap(array) {
+//   array.map((item) => {
+//     if (!isNaN(item)) {
+//    console.log(item)
+//     }
+//   });
+// }
+
+// taskMap(array);
+
+// function taskForEach() {
+//   array.forEach((item) => {
+//     if (!isNaN(item)) {
+//       let liElement = document.createElement("li");
+//       liElement.textContent = item;
+//       ul41.append(liElement);
+//     }
+//   });
+// }
+
+// taskForEach(array);
+// 4.2. Tik tekstą (string tipo duomenis).
 
 for (let i = 0; i < array.length; i++) {
   let item = array[i];
-  if (!isNaN(item)) {
-    console.log(item);
+  if (typeof item === "string") {
+    let ul42 = document.querySelector("#task-42");
+    let liElement = document.createElement("li");
+    liElement.textContent = item;
+    ul42.append(liElement);
   }
 }
-
-// array.map((item) => {
-//   if (!isNaN(item)) {
-//     console.log(item);
-//   }
-// });
-
-// array.forEach((item) => {
-//   if (!isNaN(item)) {
-//     console.log(item);
-//   }
-// });
-
-// 4.2. Tik tekstą (string tipo duomenis).
-
-// for (let i = 0; i < array.length; i++) {
-//   let item = array[i];
-//   if (typeof item === "string") {
-//     console.log(item);
-//   }
-// }
 
 // array.map((item) => {
 //   if (typeof item === "string") {
@@ -102,12 +116,15 @@ for (let i = 0; i < array.length; i++) {
 
 // 4.3. Tik skaičius (number tipo duomenis) ir juos pakelti 4-tuoju laipsniu.
 
-// for (let i = 0; i < array.length; i++) {
-//   let item = array[i];
-//   if (!isNaN(item)) {
-//     console.log(item ** 4);
-//   }
-// }
+for (let i = 0; i < array.length; i++) {
+  let item = array[i];
+  if (!isNaN(item)) {
+    let ul43 = document.querySelector("#task-43");
+    let liElement = document.createElement("li");
+    liElement.textContent = item ** 4;
+    ul43.append(liElement);
+  }
+}
 
 // array.map((item) => {
 //   if (!isNaN(item)) {
@@ -123,12 +140,15 @@ for (let i = 0; i < array.length; i++) {
 
 // 4.4. Tik skaičius (number tipo duomenis) ir prie jų pridėti 55.
 
-// for (let i = 0; i < array.length; i++) {
-//   let item = array[i];
-//   if (!isNaN(item)) {
-//     console.log(item + 55);
-//   }
-// }
+for (let i = 0; i < array.length; i++) {
+  let item = array[i];
+  if (!isNaN(item)) {
+    let ul44 = document.querySelector("#task-44");
+    let liElement = document.createElement("li");
+    liElement.textContent = item + 55;
+    ul44.append(liElement);
+  }
+}
 
 // array.map((item) => {
 //   if (!isNaN(item)) {
@@ -144,12 +164,15 @@ for (let i = 0; i < array.length; i++) {
 
 // 4.5. Tik skaičius (number tipo duomenis) ir juos padalinti iš 2.
 
-// for (let i = 0; i < array.length; i++) {
-//   let item = array[i];
-//   if (!isNaN(item)) {
-//     console.log(item / 2);
-//   }
-// }
+for (let i = 0; i < array.length; i++) {
+  let item = array[i];
+  if (!isNaN(item)) {
+    let ul45 = document.querySelector("#task-45");
+    let liElement = document.createElement("li");
+    liElement.textContent = item / 2;
+    ul45.append(liElement);
+  }
+}
 
 // array.map((item) => {
 //   if (!isNaN(item)) {
@@ -165,12 +188,15 @@ for (let i = 0; i < array.length; i++) {
 
 // 4.6. Tik skaičius (number tipo duomenis) ir prieš juos pridėti teksta, tokiu formatu: "Number: 2".
 
-// for (let i = 0; i < array.length; i++) {
-//   let item = array[i];
-//   if (!isNaN(item)) {
-//     console.log("Number: " + item);
-//   }
-// }
+for (let i = 0; i < array.length; i++) {
+  let item = array[i];
+  if (!isNaN(item)) {
+    let ul46 = document.querySelector("#task-46");
+    let liElement = document.createElement("li");
+    liElement.textContent = "Number: " + item;
+    ul46.append(liElement);
+  }
+}
 
 // array.map((item) => {
 //   if (!isNaN(item)) {
@@ -190,12 +216,15 @@ for (let i = 0; i < array.length; i++) {
 //   "Index: 2, Number: 5"
 //   Ir t.t.
 
-// for (let i = 0; i < array.length; i++) {
-//   let item = array[i];
-//   if (!isNaN(item)) {
-//     console.log("Index: " + i + "," + " Number: " + item);
-//   }
-// }
+for (let i = 0; i < array.length; i++) {
+  let item = array[i];
+  if (!isNaN(item)) {
+    let ul47 = document.querySelector("#task-47");
+    let liElement = document.createElement("li");
+    liElement.textContent = "Index: " + i + "," + " Number: " + item;
+    ul47.append(liElement);
+  }
+}
 
 // array.map(function (item, index) {
 //   if (!isNaN(item)) {
@@ -211,12 +240,15 @@ for (let i = 0; i < array.length; i++) {
 
 // 4.8. Tik skaičius (number tipo duomenis) ir juos padauginti iš jų pačių indekso.
 
-// for (let i = 0; i < array.length; i++) {
-//   let item = array[i];
-//   if (!isNaN(item)) {
-//     console.log(item * i);
-//   }
-// }
+for (let i = 0; i < array.length; i++) {
+  let item = array[i];
+  if (!isNaN(item)) {
+    let ul48 = document.querySelector("#task-48");
+    let liElement = document.createElement("li");
+    liElement.textContent = item * i;
+    ul48.append(liElement);
+  }
+}
 
 // array.map(function (item, index) {
 //   if (!isNaN(item)) {
@@ -238,19 +270,22 @@ for (let i = 0; i < array.length; i++) {
 //   - Penktą skaičių dauginti iš ketvirto.
 //   Ir t.t.
 
-// let arrayNums = array.filter(function (num) {
-//   return typeof num === "number";
-// });
+let arrayNums = array.filter(function (num) {
+  return typeof num === "number";
+});
 
-// console.log(arrayNums);
+console.log(arrayNums);
 
-// // for (let i = 0; i < arrayNums.length; i++) {
-// //   let item = arrayNums[i];
-// //   let previousNum = arrayNums[i - 1];
-// //   if (i > 0) {
-// //     console.log(item * previousNum);
-// //   }
-// // }
+for (let i = 0; i < arrayNums.length; i++) {
+  let item = arrayNums[i];
+  let previousNum = arrayNums[i - 1];
+  if (i > 0) {
+    let ul49 = document.querySelector("#task-49");
+    let liElement = document.createElement("li");
+    liElement.textContent = item * previousNum;
+    ul49.append(liElement);
+  }
+}
 
 // // arrayNums.map(function (item, i) {
 // //   let previousNum = arrayNums[i - 1];
@@ -268,19 +303,22 @@ for (let i = 0; i < array.length; i++) {
 
 // 4.10. Tik tuos skaičius (number tipo duomenis), kuriuos padauginus iš 5, atsakymas gaunasi didesnis už 350.
 
-// let arrayNums = array.filter(function (num) {
-//   return typeof num === "number";
-// });
+let arrayNums5 = array.filter(function (num) {
+  return typeof num === "number";
+});
 
-// console.log(arrayNums);
+console.log(arrayNums);
 
-// for (let i = 0; i < arrayNums.length; i++) {
-//   let item = arrayNums[i];
+for (let i = 0; i < arrayNums.length; i++) {
+  let item = arrayNums5[i];
 
-//   if (item * 5 > 350) {
-//     console.log(item);
-//   }
-// }
+  if (item * 5 > 350) {
+    let ul410 = document.querySelector("#task-410");
+    let liElement = document.createElement("li");
+    liElement.textContent = item;
+    ul410.append(liElement);
+  }
+}
 
 // arrayNums.map(function (item, i) {
 //   if (item * 5 > 350) {
@@ -296,13 +334,16 @@ for (let i = 0; i < array.length; i++) {
 
 // 4.11. Tik tekstą (string tipo duomenis) ir prie kiekvieno teksto parašyti kiek simbolių jis turi, pvz. "Start has 5 symbols".
 
-// for (let i = 0; i < array.length; i++) {
-//   let item = array[i];
-//   let length = array[i].length;
-//   if (typeof item === "string") {
-//     console.log(item + " has " + length + " symbols");
-//   }
-// }
+for (let i = 0; i < array.length; i++) {
+  let item = array[i];
+  let length = array[i].length;
+  if (typeof item === "string") {
+    let ul411 = document.querySelector("#task-411");
+    let liElement = document.createElement("li");
+    liElement.textContent = item + " has " + length + " symbols";
+    ul411.append(liElement);
+  }
+}
 
 // array.map((item) => {
 //   let length = item.length;
@@ -320,12 +361,15 @@ for (let i = 0; i < array.length; i++) {
 
 // 4.12. Tik tekstą (string tipo duomenis), tačiau kiekviena žodžio raidę išskiriant brūkšniu ir paverčiant ją didžiąja raide, pvz.: "S-T-A-R-T".
 
-// for (let i = 0; i < array.length; i++) {
-//   let item = array[i];
-//   if (typeof item === "string") {
-//     console.log(item.toUpperCase().split("").join("-"));
-//   }
-// }
+for (let i = 0; i < array.length; i++) {
+  let item = array[i];
+  if (typeof item === "string") {
+    let ul412 = document.querySelector("#task-412");
+    let liElement = document.createElement("li");
+    liElement.textContent = item.toUpperCase().split("").join("-");
+    ul412.append(liElement);
+  }
+}
 
 // array.map((item) => {
 //   if (typeof item === "string") {
@@ -340,3 +384,48 @@ for (let i = 0; i < array.length; i++) {
 // });
 
 // 4.13. Tik tekstą (string tipo duomenis), tačiau kiekviena žodžio pirmą ir trečią raidę pakeičiant brūkšniu (underscore), pvz.: "_t_rt";
+for (let i = 0; i < array.length; i++) {
+  let item = array[i];
+  if (typeof item === "string") {
+    let ul413 = document.querySelector("#task-413");
+    let liElement = document.createElement("li");
+    splitLetters = item.split("");
+    splitLetters[0] = "_";
+    splitLetters[2] = "_";
+    liElement.textContent = splitLetters.join("");
+    ul413.append(liElement);
+  }
+}
+
+// 4.14. Tik tekstą (string tipo duomenis), tačiau žodį parašant atvirkščiai, pvz.: vietoje "start" parašyti "trats";
+for (let i = 0; i < array.length; i++) {
+  let item = array[i];
+  if (typeof item === "string") {
+    let ul414 = document.querySelector("#task-414");
+    let liElement = document.createElement("li");
+    liElement.textContent = item.split("").reverse().join("");
+    ul414.append(liElement);
+  }
+}
+
+// 4.15. Tik tekstą (string tipo duomenis) ir prie kiekvieno teksto parašyti tarp kokių narių masyve jis yra, pvz.: "Word obuolys is between -5564 and -51 in the array".
+
+array.map((item, index) => {
+  if (typeof item === "string") {
+    if (index === array.length - 1) {
+      let ul415 = document.querySelector("#task-415");
+      let liElement = document.createElement("li");
+      liElement.textContent = `Word ${item} is the first in the array and after ${
+        array[index - 1]
+      } in the array`;
+      ul415.append(liElement);
+    } else {
+      let ul415 = document.querySelector("#task-415");
+      let liElement = document.createElement("li");
+      liElement.textContent = `Word ${item} is between ${
+        array[index - 1]
+      } and ${array[index + 1]} in the array`;
+      ul415.append(liElement);
+    }
+  }
+});
