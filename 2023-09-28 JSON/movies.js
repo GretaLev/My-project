6a270030b472b9f3592c430dbd95de3eba1689c8
GretaLev <levinskaite.greta@gmail.com>
2023-09-28 12:46:54 +0300

    Task movies wit ul and li elements

--- 2023-09-28 JSON/movies.js
+++ 2023-09-28 JSON/movies.js
@@ -5,19 +5,47 @@ fetch("movies.json")
   .then((data) => {
     for (let i = 0; i < data.length; i++) {
       let moviesItem = document.createElement("div");
-      moviesItem.classList.add("movie-item");
+      moviesItem.classList.add("movies-item");
       moviesList.append(moviesItem);
       let movieTitle = document.createElement("h2");
+      movieTitle.style.color = "blue";
       movieTitle.textContent = data[i].movie;
+
       moviesItem.append(movieTitle);
       let description = document.createElement("p");
-      description.textContent = `Movie description: ${data[i].description}`;
+      description.style.fontSize = "20px";
+      description.innerHTML = `<strong>Movie description:</strong> ${data[i].description}`;
       moviesItem.append(description);
-      let mainActors = document.createElement("p");
-      mainActors.textContent = `Main actors: ${data[i].actors}`;
+      //Actors ul element
+      let mainActors = document.createElement("h3");
+      mainActors.textContent = `Main actors:`;
       moviesItem.append(mainActors);
-      let moviesInfo = document.createElement("p");
-      moviesInfo.textContent = `Movie was released in ${data[i].released} by Director ${data[i].director}. Movie genre is ${data[i].genre} and is rated ${data[i].rate}.`;
-      moviesItem.append(moviesInfo);
+      let mainActorsUlElement = document.createElement("ul");
+      moviesItem.append(mainActorsUlElement);
+
+      //Actors array
+      data[i].actors.forEach((item) => {
+        let mainActorsLiElement = document.createElement("li");
+        mainActorsLiElement.textContent = item;
+        mainActorsUlElement.append(mainActorsLiElement);
+      });
+      //Genres ul element
+      let mainGenres = document.createElement("h3");
+      mainGenres.textContent = `Main genres:`;
+      moviesItem.append(mainGenres);
+      let mainGenresUlElement = document.createElement("ul");
+      moviesItem.append(mainGenresUlElement);
+
+      //   //   Genres array
+      data[i].genre.forEach((item) => {
+        let mainGenresLiElement = document.createElement("li");
+        mainGenresLiElement.textContent = item;
+        mainGenresUlElement.append(mainGenresLiElement);
+      });
+
+      //Director
+      let director = document.createElement("h3");
+      director.textContent = `Movie Director: ${data[i].director}`;
+      moviesItem.append(director);
     }
   });
